e1263b6c12648416c0918696883275b51a083d17
HackerConor <dhdkstlr@gmail.com>
2024-03-14 17:23:51 +0900

    init commit message.

---
+++ portal_dft/src/main/java/com/dft/portal/portal_dft/PortalDftApplication.java
@@ -0,0 +1,13 @@
+package com.dft.portal.portal_dft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalDftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalDftApplication.class, args);
+	}
+
+}
---
+++ portal_dft/src/test/java/com/dft/portal/portal_dft/PortalDftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dft.portal.portal_dft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalDftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
